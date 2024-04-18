a57f8676dc341653d39e15f2e8e9ec1be8626ad5
danilovalerio <danilo_vs@hotmail.com>
2024-04-18 09:07:58 -0300

    create components mapped

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { CardHeaderComponent } from './components/card-header/card-header.component';
+import { CardItemComponent } from './components/card-item/card-item.component';
+import { CardResumeComponent } from './components/card-resume/card-resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardHeaderComponent,
+    CardItemComponent,
+    CardResumeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/card-resume/card-resume.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-resume',
+  templateUrl: './card-resume.component.html',
+  styleUrl: './card-resume.component.scss'
+})
+export class CardResumeComponent {
+
+}
